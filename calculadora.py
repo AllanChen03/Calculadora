8800f557093aec2b2288e78de457d96e14ff5215
AllanChen03 <allan031199@gmail.com>
2019-10-17 10:43:11 -0600

    Se crea la funcion Suma

--- calculadora.py
+++ calculadora.py
@@ -1,16 +1,5 @@
-def Resta(Num1,Num2):
-    return Num1 - Num2
-
 def Suma(Num1,Num2):
     return Num1 + Num2
 
-def Sumatoria(Num):
-    if isinstance(Num,int):
-        return Sumatoria_aux(Num)
-    else:
-        return "No es un numero entero"
-
-def Sumatoria_aux(Num):
-    if Num == 0:
-        return 0
-    else: Num+Sumatoria_aux(Num-1)
+def Resta(Num1,Num2):
+    return Num1-Num2
