ef7d521a5c90ee79cf8db973916b0777c5b5f886
AllanChen03 <allan031199@gmail.com>
2019-10-17 11:02:55 -0600

    Cambios

--- calculadora.py
+++ calculadora.py
@@ -4,8 +4,8 @@ def Suma(Num1,Num2):
 def Resta(Num1,Num2):
     return Num1-Num2
 
-def Sumatoria(Num,i):
-    if i > Num:
+def Sumatoria(Num):
+    if Num < 0:
         return 0
     else:
-        return i+3 + Sumatoria(Num,i+1)
+        return Num + Sumatoria(Num-1)
